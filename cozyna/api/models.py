1dee0289b1d3cc407e6a75641921062a8c702068
fjcamillo <fjcamillo.dev@gmail.com>
2017-03-05 07:12:21 +0800

    working

--- cozyna/api/models.py
+++ cozyna/api/models.py
@@ -7,28 +7,50 @@ from django.db import models
 class entered(models.Model):
     timeEntered = models.DateTimeField()
     timeStep = models.IntegerField()
+    sensorType = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sensorType
+
+    # class Meta:
+    #     enters = ["timeEntered", "timeStep", 'sensorType']
 
 class sat(models.Model):
     timeSat = models.DateTimeField()
     tableNumber = models.IntegerField()
     totalNumber = models.IntegerField()
 
+    def __str__(self):
+        return tableNumber
+
 class order(models.Model):
     timeOrdered = models.DateTimeField()
     waiterNumber = models.IntegerField()
     tableNumber = models.IntegerField()
+
+    def __str__(self):
+        return tableNumber
 
 class arrived(models.Model):
     timeArrived = models.DateTimeField()
     waiterNumber = models.IntegerField()
     tableNumber = models.IntegerField()
 
+    def __str__(self):
+        return tableNumber
+
 class finished(models.Model):
     timeFinished = models.DateTimeField()
     tableNumber = models.IntegerField()
     waiterTookBill = models.IntegerField()
 
+    def __str__(self):
+        return tableNumber
+
 
 class left(models.Model):
     timeLeft = models.DateTimeField()
     tableNumber = models.IntegerField()
+
+    def __str__(self):
+        return tableNumber
--- hardware/sendData.py
+++ hardware/sendData.py
@@ -4,6 +4,144 @@ import requests
 
 data = {}
 
+urls = {
+    'table1':{
+        'startsit': 'http://127.0.0.1/api/table/1/startsit',
+        'ordered': 'http://127.0.0.1/api/table/1/ordered',
+        'arrived': 'http://127.0.0.1/api/table/1/arrived',
+        'finished': 'http://127.0.0.1/api/table/1/finished',
+        'endSit': 'http://127.0.0.1/api/table/1/endSit',
+    },
+    'table2':{
+        'startsit': 'http://127.0.0.1/api/table/2/startsit',
+        'ordered': 'http://127.0.0.1/api/table/2/ordered',
+        'arrived': 'http://127.0.0.1/api/table/2/arrived',
+        'finished': 'http://127.0.0.1/api/table/2/finished',
+        'endSit': 'http://127.0.0.1/api/table/2/endSit',
+    },
+    'table3':{
+        'startsit': 'http://127.0.0.1/api/table/3/startsit',
+        'ordered': 'http://127.0.0.1/api/table/3/ordered',
+        'arrived': 'http://127.0.0.1/api/table/3/arrived',
+        'finished': 'http://127.0.0.1/api/table/3/finished',
+        'endSit': 'http://127.0.0.1/api/table/3/endSit',
+    },
+    'table4':{
+        'startsit': 'http://127.0.0.1/api/table/4/startsit',
+        'ordered': 'http://127.0.0.1/api/table/4/ordered',
+        'arrived': 'http://127.0.0.1/api/table/4/arrived',
+        'finished': 'http://127.0.0.1/api/table/4/finished',
+        'endSit': 'http://127.0.0.1/api/table/4/endSit',
+    },
+    'table5':{
+        'startsit': 'http://127.0.0.1/api/table/5/startsit',
+        'ordered': 'http://127.0.0.1/api/table/5/ordered',
+        'arrived': 'http://127.0.0.1/api/table/6/arrived',
+        'finished': 'http://127.0.0.1/api/table/6/finished',
+        'endSit': 'http://127.0.0.1/api/table/6/endSit',
+    },
+    'table7':{
+        'startsit': 'http://127.0.0.1/api/table/7/startsit',
+        'ordered': 'http://127.0.0.1/api/table/7/ordered',
+        'arrived': 'http://127.0.0.1/api/table/7/arrived',
+        'finished': 'http://127.0.0.1/api/table/7/finished',
+        'endSit': 'http://127.0.0.1/api/table/7/endSit',
+    },
+    'table8':{
+        'startsit': 'http://127.0.0.1/api/table/8/startsit',
+        'ordered': 'http://127.0.0.1/api/table/8/ordered',
+        'arrived': 'http://127.0.0.1/api/table/8/arrived',
+        'finished': 'http://127.0.0.1/api/table/8/finished',
+        'endSit': 'http://127.0.0.1/api/table/8/endSit',
+    },
+    'table9':{
+        'startsit': 'http://127.0.0.1/api/table/9/startsit',
+        'ordered': 'http://127.0.0.1/api/table/9/ordered',
+        'arrived': 'http://127.0.0.1/api/table/9/arrived',
+        'finished': 'http://127.0.0.1/api/table/9/finished',
+        'endSit': 'http://127.0.0.1/api/table/9/endSit',
+    },
+    'table10':{
+        'startsit': 'http://127.0.0.1/api/table/10/startsit',
+        'ordered': 'http://127.0.0.1/api/table/10/ordered',
+        'arrived': 'http://127.0.0.1/api/table/10/arrived',
+        'finished': 'http://127.0.0.1/api/table/10/finished',
+        'endSit': 'http://127.0.0.1/api/table/10/endSit',
+    },
+    'table11':{
+        'startsit': 'http://127.0.0.1/api/table/11/startsit',
+        'ordered': 'http://127.0.0.1/api/table/11/ordered',
+        'arrived': 'http://127.0.0.1/api/table/11/arrived',
+        'finished': 'http://127.0.0.1/api/table/11/finished',
+        'endSit': 'http://127.0.0.1/api/table/11/endSit',
+    },
+    'table12':{
+        'startsit': 'http://127.0.0.1/api/table/12/startsit',
+        'ordered': 'http://127.0.0.1/api/table/12/ordered',
+        'arrived': 'http://127.0.0.1/api/table/12/arrived',
+        'finished': 'http://127.0.0.1/api/table/12/finished',
+        'endSit': 'http://127.0.0.1/api/table/12/endSit',
+    },
+    'table13':{
+        'startsit': 'http://127.0.0.1/api/table/13/startsit',
+        'ordered': 'http://127.0.0.1/api/table/13/ordered',
+        'arrived': 'http://127.0.0.1/api/table/13/arrived',
+        'finished': 'http://127.0.0.1/api/table/13/finished',
+        'endSit': 'http://127.0.0.1/api/table/13/endSit',
+    },
+    'table14':{
+        'startsit': 'http://127.0.0.1/api/table/14/startsit',
+        'ordered': 'http://127.0.0.1/api/table/14/ordered',
+        'arrived': 'http://127.0.0.1/api/table/14/arrived',
+        'finished': 'http://127.0.0.1/api/table/14/finished',
+        'endSit': 'http://127.0.0.1/api/table/14/endSit',
+    },
+    'table15':{
+        'startsit': 'http://127.0.0.1/api/table/15/startsit',
+        'ordered': 'http://127.0.0.1/api/table/15/ordered',
+        'arrived': 'http://127.0.0.1/api/table/15/arrived',
+        'finished': 'http://127.0.0.1/api/table/15/finished',
+        'endSit': 'http://127.0.0.1/api/table/15/endSit',
+    },
+    'table16':{
+        'startsit': 'http://127.0.0.1/api/table/16/startsit',
+        'ordered': 'http://127.0.0.1/api/table/16/ordered',
+        'arrived': 'http://127.0.0.1/api/table/16/arrived',
+        'finished': 'http://127.0.0.1/api/table/16/finished',
+        'endSit': 'http://127.0.0.1/api/table/16/endSit',
+    },
+    'table17':{
+        'startsit': 'http://127.0.0.1/api/table/17/startsit',
+        'ordered': 'http://127.0.0.1/api/table/17/ordered',
+        'arrived': 'http://127.0.0.1/api/table/17/arrived',
+        'finished': 'http://127.0.0.1/api/table/17/finished',
+        'endSit': 'http://127.0.0.1/api/table/17/endSit',
+    },
+    'table18':{
+        'startsit': 'http://127.0.0.1/api/table/1/startsit',
+        'ordered': 'http://127.0.0.1/api/table/18/ordered',
+        'arrived': 'http://127.0.0.1/api/table/18/arrived',
+        'finished': 'http://127.0.0.1/api/table/18/finished',
+        'endSit': 'http://127.0.0.1/api/table/18/endSit',
+    },
+    'table19':{
+        'startsit': 'http://127.0.0.1/api/table/19/startsit',
+        'ordered': 'http://127.0.0.1/api/table/19/ordered',
+        'arrived': 'http://127.0.0.1/api/table/19/arrived',
+        'finished': 'http://127.0.0.1/api/table/19/finished',
+        'endSit': 'http://127.0.0.1/api/table/19/endSit',
+    },
+    'table20':{
+        'startsit': 'http://127.0.0.1/api/table/20/startsit',
+        'ordered': 'http://127.0.0.1/api/table/20/ordered',
+        'arrived': 'http://127.0.0.1/api/table/20/arrived',
+        'finished': 'http://127.0.0.1/api/table/20/finished',
+        'endSit': 'http://127.0.0.1/api/table/20/endSit',
+    },
+
+
+}
+
 arduino = Serial('/dev/ttyACM0', 9600, timeout=.1)
 
 def segregate(config):
